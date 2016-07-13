c3e9ecdffb96116ced691b6c916b9c335c33ad8f
Simon Lindsay <simon.lindsay@adelaide.edu.au>
2016-07-13 12:32:27 +0930

    Use the box specified in the site manager Squashed commit of the following:
    Merged feature branch 'feature/wcms-allow-other-box-types'
    
    commit bb605659b69ce414658139e0cd673ff402f15521
    Author: Simon Lindsay <simon.lindsay@adelaide.edu.au>
    Date:   Tue Jul 12 15:40:43 2016 +0930
    
        Update defaults to pass in the correct box type

--- UASMContentGenerate.php
+++ UASMContentGenerate.php
@@ -124,7 +124,7 @@ $distribution = Node::create([
   'status' => 1,
   'title' => 'WCMS D8 Dev Distribution',
   'field_ua_sm_git_repository' => [['value' => 'git@gitlab.adelaide.edu.au:web-team/ua-wcms-d8.git']],
-  'field_ua_sm_box_type' =>       [['value' => 'web']],
+  'field_ua_sm_box_type' =>       [['value' => 'uofa/apache2-php7']],
 ]);
 $distribution->save();
 
