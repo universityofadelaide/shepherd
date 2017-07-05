22dffe61e9c70050b3eb2394b68dd446deb304fe
Chris Barton <c.barton@adelaide.edu.au>
2017-07-05 13:39:34 +0930

    Fix site titles and add breadcrumbs to site/ specific pages for context.
    Squashed commit of the following:
    
    Merged feature branch 'feature/fix-sites-views'
    
    commit 137961cada4b3d81c8732f586867273618dfd4b3
    Author: Chris Barton <c.barton@adelaide.edu.au>
    Date:   Wed Jul 5 10:10:21 2017 +0930
    
        Make breadcrumbs
    
    commit 020afdda2267033676cedab523f7e0839565a5ca
    Author: Chris Barton <c.barton@adelaide.edu.au>
    Date:   Tue Jul 4 15:44:38 2017 +0930
    
        Learning about Breadcrumb builders
    
    commit 2ad063bc714597749b6fd556142505681992214a
    Author: Chris Barton <c.barton@adelaide.edu.au>
    Date:   Tue Jul 4 15:32:12 2017 +0930
    
        Begin breadcrumb creation .. this should attempt to fix nav issues
    
    commit 1f9ecb4a4331f3fa56927528072fd36c09c0ff13
    Author: Chris Barton <c.barton@adelaide.edu.au>
    Date:   Tue Jul 4 15:04:38 2017 +0930
    
        Add sites with context

---
+++ web/modules/custom/shp_content_types/src/Breadcrumb/SitesBreadcrumbBuilder.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\shp_content_types\Breadcrumb;
+
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Link;
+use Drupal\node\Entity\Node;
+
+/**
+ * Class SitesBreadcrumbBuilder
+ * @package Drupal\shp_content_types\Breadcrumb
+ */
+class SitesBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+  /**
+   * Definition of view_ids that relate to a site.
+   *
+   * @var array
+   */
+  protected $sitesViews = [
+    'shp_site_environments' => 'Environments',
+    'shp_site_users' => 'Users',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $parameters = $route_match->getParameters()->all();
+    // Be more specific.
+    if (array_key_exists('node', $parameters) && !empty($parameters['node'])
+      && is_object($parameters['node'])) {
+      $node = $parameters['node']->getType();
+      return in_array($node, ["shp_site"]) ? TRUE : FALSE;
+    }
+    if (array_key_exists('view_id', $parameters) && !empty($parameters['view_id'])) {
+      return in_array($parameters['view_id'], array_keys($this->sitesViews)) ? TRUE : FALSE;
+    }
+    // Handle view specific stuff here.
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+    $breadcrumb->addLink(Link::createFromRoute('Sites', 'shp_custom.sites'));
+    $parameters = $route_match->getParameters()->all();
+    $node = is_object($parameters['node']) ? $parameters['node'] : Node::load($parameters['node']);
+    $node_type = $node->bundle();
+
+    switch ($node_type) {
+      case 'shp_site':
+        $breadcrumb->addLink(Link::createFromRoute($node->getTitle(), 'entity.node.canonical', ['node' => $node->id()]));
+        break;
+    }
+
+    if (array_key_exists('view_id', $parameters)) {
+      $breadcrumb->addLink(Link::createFromRoute($this->sitesViews[$parameters['view_id']], $route_match->getRouteName(), $parameters));
+    }
+
+    $breadcrumb->addCacheContexts(['route']);
+    return $breadcrumb;
+  }
+
+}
