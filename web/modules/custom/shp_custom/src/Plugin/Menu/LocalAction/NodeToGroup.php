75a53a1b83da40e2d684cbc741c01c7c7f904c06
Casey Fulton <casey.fulton@adelaide.edu.au>
2017-07-05 11:57:17 +0930

    WEBOPS-144 User groups

---
+++ web/modules/custom/shp_custom/src/Plugin/Menu/LocalAction/NodeToGroup.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\shp_custom\Plugin\Menu\LocalAction;
+
+use Drupal\group\Plugin\Menu\LocalAction\WithDestination;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Routing\RouteMatchInterface;
+
+/**
+ * Defines a local action plugin with a dynamic title.
+ */
+class NodeToGroup extends WithDestination {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRouteParameters(RouteMatchInterface $route_match) {
+    $parameters = isset($this->pluginDefinition['route_parameters']) ? $this->pluginDefinition['route_parameters'] : [];
+    $route = $this->routeProvider->getRouteByName($this->getRouteName());
+    $variables = $route->compile()->getVariables();
+
+    foreach ($variables as $name) {
+      if ($name == 'group') {
+        $id = $route_match->getRawParameter('node');
+        $node = Node::load($id);
+
+        /** @var \Drupal\group\Entity\GroupInterface $group */
+        $group = \Drupal::service('shp_content_types.site_group_manager')->load($node);
+        $parameters['group'] = $group->id();
+      }
+    }
+
+    return $parameters;
+  }
+
+}
