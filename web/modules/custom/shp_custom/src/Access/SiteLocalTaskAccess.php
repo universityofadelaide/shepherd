640b3dd1cbbfcf29bee1010be70b21916ee2570c
Chris Barton <c.barton@adelaide.edu.au>
2017-07-07 14:05:17 +0930

    Three attempts at setting the display .. none work so far ...

---
+++ web/modules/custom/shp_custom/src/Access/SiteLocalTaskAccess.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\shp_site\Access;
+
+use Drupal\Core\Routing\Access\AccessInterface;
+
+/**
+ * Class SiteLocalTaskAccess
+ * @package Drupal\shp_site\Access
+ */
+class SiteLocalTaskAccess implements AccessInterface {
+
+  public function access() {
+
+  }
+
+}
