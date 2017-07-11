7bf5e63957935d645870b1730d59ec1c6ddac0fc
Chris Barton <c.barton@adelaide.edu.au>
2017-07-11 15:41:55 +0930

    How do I set theme ? Looks like I have to roll my own template

--- web/modules/custom/shp_stats/src/Controller/SitesStatsController.php
+++ web/modules/custom/shp_stats/src/Controller/SitesStatsController.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\shp_stats\Controller;
 
+use Drupal\charts\Services\ChartsSettingsService;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\shp_stats\Service\Storage;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -12,15 +13,25 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class SitesStatsController extends ControllerBase {
 
   /**
+   * Storage service to retrieve stats from database.
+   *
    * @var \Drupal\shp_stats\Service\Storage
    */
   protected $storageService;
 
   /**
+   * Chart settings.
+   *
+   * @var \Drupal\charts\Services\ChartsSettingsService
+   */
+  protected $chartSettings;
+
+  /**
    * SitesStatsController constructor.
    */
-  public function __construct(Storage $storage) {
+  public function __construct(Storage $storage, ChartsSettingsService $chartSettings) {
     $this->storageService = $storage;
+    $this->chartSettings = $chartSettings;
   }
 
   /**
@@ -28,7 +39,7 @@ class SitesStatsController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('shp_stats.storage')
+      $container->get('shp_stats.storage'),
       $container->get('charts.settings')
     );
   }
@@ -37,13 +48,17 @@ class SitesStatsController extends ControllerBase {
    * Render a list of entries in the database.
    */
   public function display() {
+
+    $library = $this->chartSettings['library'];
+    $options = [];
+
     $entries = $this->storageService->load();
     $rows = [];
     foreach ($entries as $entry) {
       $rows[] = array_map('Drupal\Component\Utility\SafeMarkup::checkPlain', (array) $entry);
     }
     $markup['table'] = [
-      '#type' => 'table',
+      '#theme' => 'charts',
       '#rows' => $rows,
       '#empty' => 'No data available',
     ];
