04f7d7e2eec7fcca5fe0273dda127f0f7678060d
Chris Barton <c.barton@adelaide.edu.au>
2017-07-11 10:43:21 +0930

    Create a controller and inject the service

---
+++ web/modules/custom/shp_stats/src/Controller/SitesStatsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\shp_stats\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\shp_stats\Service\Storage;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class SitesStatsController
+ */
+class SitesStatsController extends ControllerBase {
+
+  /**
+   * @var \Drupal\shp_stats\Service\Storage
+   */
+  protected $storageService;
+
+  /**
+   * SitesStatsController constructor.
+   */
+  public function __construct(Storage $storage) {
+    $this->storageService = $storage;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('shp_stats.storage')
+    );
+  }
+
+  /**
+   * Render a list of entries in the database.
+   */
+  public function display() {
+    $entries = $this->storageService->load();
+    $rows = [];
+    foreach ($entries as $entry) {
+      $rows[] = array_map('Drupal\Component\Utility\SafeMarkup::checkPlain', (array) $entry);
+    }
+    $markup['table'] = [
+      '#type' => 'table',
+      '#rows' => $rows,
+      '#empty' => 'No data available',
+    ];
+
+    return $markup;
+  }
+
+}
