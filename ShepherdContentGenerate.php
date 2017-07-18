1044cba36945d0a086b65113ccc83ed91592c785
Casey Fulton <casey.fulton@adelaide.edu.au>
2017-07-18 16:50:08 +0930

    WEBOPS-40 mechanism for restricting access based on distro

--- ShepherdContentGenerate.php
+++ ShepherdContentGenerate.php
@@ -8,6 +8,7 @@
 use Drupal\node\Entity\Node;
 use Drupal\shp_orchestration\Entity\OpenShiftConfigEntity;
 use Drupal\taxonomy\Entity\Term;
+use Drupal\user\Entity\User;
 
 $domain_name = getenv("OPENSHIFT_DOMAIN") ?: '192.168.99.100.nip.io';
 $openshift_url = getenv("OPENSHIFT_URL") ?: 'https://192.168.99.100:8443';
@@ -105,3 +106,19 @@ $env = Node::create([
   'field_shp_site'             => [['target_id' => $site->id()]],
 ]);
 $env->save();
+
+$oc_user_result = \Drupal::entityTypeManager()->getStorage('user')->loadByProperties(['name' => 'oc']);
+/** @var \Drupal\user\Entity\User $oc_user */
+$oc_user = $oc_user_result ? reset($oc_user_result) : FALSE;
+if (!$oc_user) {
+  $oc_user = User::create([
+    'name' => 'oc',
+    'pass' => 'password',
+    'status' => 1,
+  ]);
+  $oc_user->save();
+}
+
+/** @var \Drupal\group\Entity\GroupInterface $dist_group */
+$dist_group = \Drupal::service('shp_content_types.group_manager')->load($distribution);
+$dist_group->addMember($oc_user, ['group_roles' => ['shp_distribution-online-consulta']]);
