ed1b0f3e397f3983f4e80384f3cf49dc8190e989
Michael Priest <pingers@gmail.com>
2017-07-19 16:13:06 +0930

    WEBOPS-217 Provides an endpoint for Shepherd managed sites to fetch user roles.
    * Added a third party setting to group role config entities to store synced role.
    
    Squashed commit of the following:
    
    Merged feature branch 'feature/webops-217-user-roles'
    
    commit 8d4c3c6bc7beb24db5144d1163df41ccab2d0e03
    Author: Michael Priest <pingers@gmail.com>
    Date:   Wed Jul 19 15:15:43 2017 +0930
    
        Remove crud.
    
    commit ae7fcc8c8b43261f545a78ad872d674d73ff6676
    Author: Michael Priest <pingers@gmail.com>
    Date:   Wed Jul 19 15:15:14 2017 +0930
    
        Get the new third party setting and return it at the endpoint... after using all the array built-ins.
    
    commit ddf1deb5c9f468374323fd8f13c8e7b4ba462183
    Author: Michael Priest <pingers@gmail.com>
    Date:   Wed Jul 19 15:04:43 2017 +0930
    
        Fix the config to have a dependency... and the corresponding module deps.
    
    commit 522d00b5db6bb6bc2cce905817192a3f3ce4e7f8
    Author: Michael Priest <pingers@gmail.com>
    Date:   Wed Jul 19 14:25:46 2017 +0930
    
        Add some dependencies to all of the custom modules. End this madness.
    
    commit 8468ac4dc8d7c0274a6a3f6b90cfaedcf046311f
    Author: Michael Priest <pingers@gmail.com>
    Date:   Wed Jul 19 13:44:18 2017 +0930
    
        Enable the shp_roles module by default.
    
    commit 43e68c0cca316ef79b81ea5e4ddad3786c1651b5
    Author: Michael Priest <pingers@gmail.com>
    Date:   Wed Jul 19 13:42:19 2017 +0930
    
        Adds the logic to support adding third party settings to group role config entity.
    
    commit eae865b9a6e5a20bf64b4353444be1b729f16ced
    Author: Michael Priest <pingers@gmail.com>
    Date:   Wed Jul 19 13:38:16 2017 +0930
    
        Add the schema and updated config entity exports for group roles to include a third party setting to store the synced role - as 32 char config id's are too short to be able to store the synced role.
    
    commit 936c6fe7915bb426b1436bf42901945331f495d0
    Author: Michael Priest <pingers@gmail.com>
    Date:   Wed Jul 19 11:20:28 2017 +0930
    
        Restrict site users view to only site groups.
    
    commit 23d065a5962c7d29297fbc07c6b9f491ba4ac67d
    Author: Michael Priest <pingers@gmail.com>
    Date:   Wed Jul 19 09:12:09 2017 +0930
    
        Fix the array key merging.
    
    commit a5828aed94142886388b0d266c8cd4ada966f064
    Author: Michael Priest <pingers@gmail.com>
    Date:   Wed Jul 19 08:54:58 2017 +0930
    
        Get the roles.
    
    commit 1312d0bc28d0ab55f3da5d8a6d01edb75ce5aade
    Author: Michael Priest <pingers@gmail.com>
    Date:   Mon Jul 17 16:37:59 2017 +0930
    
        Got the route parameters loading!
    
    commit 0d0d90e11e78f78dcc5682c69ee9967a8bb175aa
    Author: Michael Priest <pingers@gmail.com>
    Date:   Mon Jul 17 16:23:25 2017 +0930
    
        Ever closer.
    
    commit 5e048c23a4219ef8f2165e728db58d3901c40e8e
    Author: Michael Priest <pingers@gmail.com>
    Date:   Fri Jul 14 16:05:45 2017 +0930
    
        Stub out a paramconverter to convert a username to a user entity.
    
    commit a4a69be1b88d6d9dc7cf4e1ae1c79fe5aae4cf89
    Author: Michael Priest <pingers@gmail.com>
    Date:   Fri Jul 14 14:58:03 2017 +0930
    
        Add a route.
    
    commit 8ada49b7cd206bf17318516ca9b4714f51811534
    Author: Michael Priest <pingers@gmail.com>
    Date:   Fri Jul 14 14:54:16 2017 +0930
    
        Create the module scaffold.

---
+++ web/modules/custom/shp_roles/src/ParamConverter/UserNameConverter.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\shp_roles\ParamConverter;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\ParamConverter\EntityConverter;
+use Drupal\Core\ParamConverter\ParamNotConvertedException;
+use Drupal\Core\TypedData\TranslatableInterface;
+use Symfony\Component\Routing\Route;
+
+/**
+ * Parameter converter for upcasting entity IDs to full objects.
+ */
+class UserNameConverter extends EntityConverter {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convert($value, $definition, $name, array $defaults) {
+    $entity_type_id = $this->getEntityTypeFromDefaults($definition, $name, $defaults);
+    if ($storage = $this->entityManager->getStorage($entity_type_id)) {
+      $entities = $storage->loadByProperties(['name' => $value]);
+      $entity = reset($entities);
+      // If the entity type is translatable, ensure we return the proper
+      // translation object for the current context.
+      if ($entity instanceof EntityInterface && $entity instanceof TranslatableInterface) {
+        $entity = $this->entityManager->getTranslationFromContext($entity, NULL, ['operation' => 'entity_upcast']);
+      }
+      return $entity;
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies($definition, $name, Route $route) {
+    if (!empty($definition['type']) && strpos($definition['type'], 'username:user') === 0) {
+      $entity_type_id = 'user';
+      if (strpos($definition['type'], '{') !== FALSE) {
+        $entity_type_slug = substr($entity_type_id, 1, -1);
+        return $name != $entity_type_slug && in_array($entity_type_slug, $route->compile()->getVariables(), TRUE);
+      }
+      return $this->entityManager->hasDefinition($entity_type_id);
+    }
+    return FALSE;
+  }
+
+  /**
+   * Determines the entity type ID given a route definition and route defaults.
+   *
+   * @param mixed $definition
+   *   The parameter definition provided in the route options.
+   * @param string $name
+   *   The name of the parameter.
+   * @param array $defaults
+   *   The route defaults array.
+   *
+   * @return string
+   *   The entity type ID.
+   *
+   * @throws \Drupal\Core\ParamConverter\ParamNotConvertedException
+   *   Thrown when the dynamic entity type is not found in the route defaults.
+   */
+  protected function getEntityTypeFromDefaults($definition, $name, array $defaults) {
+    $entity_type_id = 'user';
+
+    // If the entity type is dynamic, it will be pulled from the route defaults.
+    if (strpos($entity_type_id, '{') === 0) {
+      $entity_type_slug = substr($entity_type_id, 1, -1);
+      if (!isset($defaults[$entity_type_slug])) {
+        throw new ParamNotConvertedException(sprintf('The "%s" parameter was not converted because the "%s" parameter is missing', $name, $entity_type_slug));
+      }
+      $entity_type_id = $defaults[$entity_type_slug];
+    }
+    return $entity_type_id;
+  }
+
+}
