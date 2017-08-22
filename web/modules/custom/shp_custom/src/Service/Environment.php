6f8ec27b5958250e3d6b0374034267e3192a5465
Chris Barton <c.barton@adelaide.edu.au>
2017-08-22 11:44:24 +0930

    Declare our service

--- web/modules/custom/shp_custom/src/Service/Environment.php
+++ web/modules/custom/shp_custom/src/Service/Environment.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\shp_custom\Service;
 
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
 /**
  * Environment service. Provides methods to handle Environment entities.
  *
@@ -10,9 +13,32 @@ namespace Drupal\shp_custom\Service;
 class Environment {
 
   /**
-   * Apply alterations to node add form.
+   * Request service.
+   *
+   * @var RequestStack
    */
-  public function alterNodeAddForm() {
+  protected $requestStack;
+
+  /**
+   * Environment constructor.
+   *
+   * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+   *    Request stack service.
+   */
+  public function __construct(RequestStack $requestStack) {
+    $this->requestStack = $requestStack;
+  }
+
+  /**
+   * Apply alterations to node add form.
+   *
+   * @param array $form
+   *   Form render array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Form State.
+   */
+  public function alterNodeAddForm(array &$form, FormStateInterface $form_state) {
+    $test = 'here';
     // @todo If query string autcomplete field.
     // @todo Add states api to fields.
   }
