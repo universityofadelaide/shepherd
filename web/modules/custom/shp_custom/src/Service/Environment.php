a3c34cec9c56bd0ec134a881ccacb6deadd385e2
Chris Barton <c.barton@adelaide.edu.au>
2017-08-22 11:24:32 +0930

    To alter the form we need a hook, service and yes we should test this

---
+++ web/modules/custom/shp_custom/src/Service/Environment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\shp_custom\Service;
+
+/**
+ * Environment service. Provides methods to handle Environment entities.
+ *
+ * @package Drupal\shp_custom\Service
+ */
+class Environment {
+
+  /**
+   * Apply alterations to node add form.
+   */
+  public function alterNodeAddForm() {
+    // @todo If query string autcomplete field.
+    // @todo Add states api to fields.
+  }
+
+}
