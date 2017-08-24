6e6fb76cedf1350a838ed99a365c465cf54b3aa7
Chris Barton <c.barton@adelaide.edu.au>
2017-08-24 14:52:45 +0930

    Attach our behaviour .. looks like we can sub to events in drupal core that handle form updates/changes

--- web/modules/custom/shp_custom/js/app/site.js
+++ web/modules/custom/shp_custom/js/app/site.js
@@ -3,6 +3,14 @@
   // @todo - get the names for the field ?
   Drupal.behaviors.shp_short_name = {
     attach: function(context, settings) {
+      var $context = $(context);
+      var $name_field = $context.find(':input[name="title[0][value]"]');
+      var $short_name_field = $context.find(':input[name="field_shp_short_name[0][value]"]');
+      debugger;
+      // This event is triggered by drupal.form library.
+      $name_field.on('formUpdated', function (element) {
+        alert('Changed');
+      });
     }
   };
 })(jQuery, Drupal, this, this.document);
