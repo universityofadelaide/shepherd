32a28dd0aa58b6d01ea69348feaea26a5017c59b
Chris Barton <c.barton@adelaide.edu.au>
2017-08-24 16:12:44 +0930

    Cool that works, next step

--- web/modules/custom/shp_custom/js/app/site.js
+++ web/modules/custom/shp_custom/js/app/site.js
@@ -1,16 +1,37 @@
 (function ($, Drupal, window, document) {
-  // @todo - how do attach behaviour to a field.
-  // @todo - get the names for the field ?
-  Drupal.behaviors.shp_short_name = {
+
+  Drupal.behaviors.shp_site = {
     attach: function(context, settings) {
-      var $context = $(context);
-      var $name_field = $context.find(':input[name="title[0][value]"]');
-      var $short_name_field = $context.find(':input[name="field_shp_short_name[0][value]"]');
-      debugger;
-      // This event is triggered by drupal.form library.
-      $name_field.on('formUpdated', function (element) {
-        alert('Changed');
-      });
+      var $context = $(context).find('form').once('shp-site');
+      if ($context.length) {
+        var $name_field = $context.find(':input[name="title[0][value]"]');
+        var $short_name_field = $context.find(':input[name="field_shp_short_name[0][value]"]');
+        // This event is triggered by drupal.form library.
+        $name_field.on('formUpdated', function() { setShortName($short_name_field, this.value) });
+      }
     }
   };
+
+  var regex = /(\s|\_)/;
+
+  /**
+   * Update the short_name with the text from the title input.
+   * @param $element
+   * @param text
+   */
+  function setShortName($element, text) {
+    // Before we set the text. Run our text replace over the top
+    $element.val(replaceText(text));
+  }
+
+  /**
+   * Replaces all spaces, underscores and converts to lower case.
+   *
+   * @param text
+   * @returns {string}
+   */
+  function replaceText(text) {
+    return (text ? text.replace(regex, '-').toLowerCase().trim() : '');
+  }
+
 })(jQuery, Drupal, this, this.document);
