88267e21fcb9d67b53b3e763169f26662c0a0770
Chris Barton <cjbarton01@gmail.com>
2017-09-12 10:11:04 +0930

    Feature/fix site environment view (#19)
    * Add readme for setting up development. Update package json. Begin refactor of table component.
    
    * Add jsconfig.json for vscode users. Continue refactor, break out into methods. Apply JSDoc to functions and declare types.
    
    * Make requests timeout after 10 seconds. Make failed requests trigger a polling disconnection.
    
    * Add line
    
    * Oops axios.get is url then options object
    
    * Change README to DEVELOPERS
    
    * Make the poll reconnect time a const
    
    * Make the unchange state disconnect 15, this should wait a little while before disconnecting. Sometimes builds can take a little while.

--- web/modules/custom/shp_vue_view/js/src/table/Table.vue
+++ web/modules/custom/shp_vue_view/js/src/table/Table.vue
@@ -15,15 +15,23 @@
 
 <script>
   // We can assume that the drupalSettings are here.
+  // 10 second response timeout.
+  const AJAX_REQUEST_TIMEOUT = 10000;
+  const UNCHANGED_STATE_DISCONNECT = 15;
+  const FAILED_REQUEST_DISCONNECT = 3;
+  const POLL_UPDATE_CYCLE = drupalSettings.vue_table.update_cycle;
+  const POLL_RECONNECT_TIMEOUT = 900000;
+
   let poller;
-  let pollTiming = drupalSettings.vue_table.update_cycle;
+  let pollTiming = POLL_UPDATE_CYCLE;
+  let reconnectPollerTimer = null;
   let field_options = drupalSettings.vue_table.field_options;
   let field_apis = [];
   let previous_state = [];
-  const UNCHANGED_STATE_DISCONNECT = 5;
   // Store the number of unchanged calls.
   // Once this reaches the number defined, we hang up on the ajax calls.
   let disconnect_count = 0;
+
   Object.keys(field_options).forEach( (value, key) => {
     if (field_options[value].active && field_options[value].url_endpoint.length > 0) {
       field_apis.push({
@@ -37,8 +45,9 @@
   /**
    * Recurse downwards for specified property in an Array|Object.
    *
-   * @param data Object
-   * @param prop The property to search for.
+   * @param {Object} data - The object
+   * @param {string} prop - The property to search for.
+   * @returns {string} - The string
    */
   function findProp (data, prop) {
     return data.map((v) => {
@@ -46,6 +55,22 @@
       if (typeof v[prop] !== "undefined") {
         return v[prop]
       }
+    });
+  }
+
+  /**
+   * Parses the returned data from the ajax requests.
+   *
+   * @param {Object} results - Object containing results from request.
+   */
+  function parseResults(results) {
+    return results.map((v, i) => {
+      if (typeof v.data === "string") {
+        return v.data[field_apis[i].prop];
+      }
+      // Assume we have and object to deal with
+      // use a utility method to recursively find the value we want.
+      return findProp(v.data, field_apis[i].prop);
     });
   }
 
@@ -67,38 +92,55 @@
       },
       methods: {
         /**
-         * Performs an ajax request
+         * Performs ajax request(s) for all field(s).
          */
         poll() {
           // We have X number of urls to hit. Map over and get an array of promise objects.
           let requests = field_apis.map((v, i) => {
-            return Axios.get(field_apis[i].url);
+            return Axios.get(field_apis[i].url, {
+              // Set the timeout.
+              timeout: AJAX_REQUEST_TIMEOUT
+            });
           });
-
           // @todo - set the show property to show a spinner
           // but only if the current value is unknown.
-
           // When they are all done, we can unpack them.
           Axios.all(requests)
             .then((results) => {
-                let data = results.map((v, i) => {
-
-                  if (typeof v.data === "string") {
-                    return v.data[field_apis[i].prop];
-                  }
-                  // Assume we have and object to deal with
-                  // use a utility method to recursively find the value we want.
-                  return findProp(v.data, field_apis[i].prop);
-
-                });
+                let data = parseResults(results);
                 // Call update, which will handle updating the model.
                 this.update(data);
+          })
+          /**
+           * Catch errors with the multiple concurrent requests.
+           */
+          .catch((error) => {
+            /** @type {string} - Message to send to dialog */
+            let msg;
+            if (error.response) {
+              msg = `Error :
+              The server responed with :  ${error.response.status}
+              Message : ${error.response.data}
+              `;
+            } else if (error.request) {
+              msg = `Error : ${error.request}`;
+            } else {
+              msg = `Error : ${error.message}`;
+            }
+            // Log out error.
+            console.error(msg);
+            disconnect_count++;
+            // Terminate the connect if we reach a limit.
+            if (disconnect_count >= FAILED_REQUEST_DISCONNECT) {
+              this.disconnectPollingTimer('Maximum failed requests reached.');
+              this.reconnectPollingTimer();
+            }
           });
         },
         /**
          * Updates the view
          *
-         * @param data
+         * @param {Object} data - The current state.
          */
         update(data) {
           this.checkPreviousState(data);
@@ -118,7 +160,7 @@
          * If previous state is equal to current state, increment the disconnect counter and throttle the timer.
          * Once limit is reached, disconnect polling for 15 minutes.
          *
-         * @param data
+         * @param {Object} data - Current state.
          */
         checkPreviousState(data) {
           // Use underscores comparison method for objects/multidimensional arrays.
@@ -128,17 +170,8 @@
             this.throttlePoll();
             disconnect_count++;
             if (disconnect_count >= UNCHANGED_STATE_DISCONNECT) {
-              clearInterval(poller);
-              poller = null;
-              console.info('Disconnecting polling, no new state changes detected. Trying again in 15 minutes');
-              let context = this;
-              setInterval(() => {
-                console.info('Restarting polling');
-                pollTiming = drupalSettings.vue_table.update_cycle;
-                disconnect_count = 0;
-                poller = setInterval(() => { context.poll(); }, pollTiming);
-              }, 900000);
-
+              this.disconnectPollingTimer('No new state changes detected.');
+              this.reconnectPollingTimer();
             }
           }
           previous_state = data;
@@ -151,6 +184,33 @@
           pollTiming =  ( pollTiming * 2 ); // Double out the time.
           clearInterval(poller);
           poller = setInterval(() => { this.poll(); }, pollTiming);
+        },
+        /**
+         * Disconnect the polling interval.
+         *
+         * @param {string} msg - Log message giving reason for disconnection.
+         */
+        disconnectPollingTimer(msg) {
+          clearInterval(poller);
+          poller = null;
+          console.info('Disconnecting polling : ' + msg);
+        },
+        /**
+         * Reconnect polling timer.
+         */
+        reconnectPollingTimer() {
+          let context = this;
+          // Clear any previous timer id.
+          if (reconnectPollerTimer) {
+            clearTimeout(reconnectPollerTimer);
+            reconnectPollerTimer = null;
+          }
+          reconnectPollerTimer = setTimeout(() => {
+            console.info('Restarting polling');
+            pollTiming = POLL_UPDATE_CYCLE;
+            disconnect_count = 0;
+            poller = setInterval(() => { context.poll(); }, pollTiming);
+          }, POLL_RECONNECT_TIMEOUT);
         }
       }
     }
