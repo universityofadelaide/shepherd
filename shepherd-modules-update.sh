a5452f58d0b04dd7a7f0bd63550d8a74f30d5f47
Simon Lindsay <simon.lindsay@adelaide.edu.au>
2017-09-15 10:57:01 +0930

    Make public repo ready for consumption on internal projects. * Move shepherd modules into a subdirectory * Add composer.json so the shepherd module set can be used in composer.json * Add splitsh-lite script to update the shepherd-modules repo * Remove the last UA specific part - the profile

---
+++ shepherd-modules-update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+#
+# This script automates the creation and updating of a subtree split
+# in a second repository.
+#
+set -euo pipefail
+IFS=$'\n\t'
+
+source_repository=git@github.com:universityofadelaide/shepherd.git
+source_branch=develop
+source_dir=web/modules/custom/shepherd
+
+# This is typically the name you will be releasing the sub modules as.
+destination_dir_name=shepherd
+destination_repository=git@github.com:universityofadelaide/shepherd-modules.git
+destination_branch=develop
+
+temp_repo=$(mktemp -d)
+temp_branch=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-8} | head -n 1 || true)
+
+# Checkout the old repository, make it safe and checkout a branch
+git clone ${source_repository} ${temp_repo}
+cd ${temp_repo}
+git checkout ${source_branch}
+git remote remove origin
+git checkout -b ${temp_branch}
+
+# Create the split, check it out and then push the temp branch up
+sha1=$(splitsh-lite --prefix=${source_dir}:${destination_dir_name} --quiet)
+git reset --hard ${sha1}
+git remote add remote ${destination_repository}
+git push -f -u remote ${temp_branch}:${destination_branch}
+
+# Cleanup
+cd /tmp
+rm -rf ${temp_repo}
