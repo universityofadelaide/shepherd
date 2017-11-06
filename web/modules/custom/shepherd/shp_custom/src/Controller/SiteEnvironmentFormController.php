a0d388a5b71816cca441e517794e74706532c25d
Chris Barton <c.barton@adelaide.edu.au>
2017-11-06 16:22:26 +1030

    Maybe add to submit handler..

--- web/modules/custom/shepherd/shp_custom/src/Controller/SiteEnvironmentFormController.php
+++ web/modules/custom/shepherd/shp_custom/src/Controller/SiteEnvironmentFormController.php
@@ -3,7 +3,6 @@
 namespace Drupal\shp_custom\Controller;
 
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Url;
 
 /**
  * Class SiteEnvironmentFormController
@@ -13,14 +12,16 @@ class SiteEnvironmentFormController {
   /**
    * Alters a forms state to redirect back the site environments view.
    *
+   * @param array $form
+   *   Form render array.
    * @param \Drupal\Core\Form\FormStateInterface $form_state
    *   Current form state.
    */
-  public static function setRedirect(FormStateInterface $form_state) {
+  public static function setRedirect(array $form, FormStateInterface $form_state) {
     $site_id = \Drupal::routeMatch()->getRawParameters()->get('site_id');
-    $form_state->setRedirectUrl(Url::fromRoute('view.shp_site_environments.page_1', [
+    $form_state->setRedirect('view.shp_site_environments.page_1', [
       'node' => $site_id,
-    ]));
+    ]);
   }
 
 }
