31131871e2fa4a72fe7fd4b7d80c83e3bc110ed1
Michael Priest <pingers@gmail.com>
2017-12-13 11:18:37 +1030

    WEBOPS-274 Verify orchestration provider details on the settings page.
    Squashed commit of the following:
    
    Merged feature branch 'feature/webops-274-verification-of-provider-details'
    
    commit f48bbbdda1b265eb911bd53db5dd82d79d7699dd
    Author: Michael Priest <pingers@gmail.com>
    Date:   Tue Dec 12 15:21:43 2017 +1030
    
        TLS setting relates to the endpoint, move closer. TLS form error doesn't show on the form - just use endpoint. Give some clue about what to do for wrong endpoint or TLS setting.
    
    commit a2a7e22ac225f1cbd4d53aec2fc3433b7d6aad66
    Author: Chris Barton <c.barton@adelaide.edu.au>
    Date:   Mon Dec 4 14:29:44 2017 +1030
    
        Place the parent call to validate at the top.
    
    commit 57f35b58f2cc60770bc0982b6a9ca0831afea901
    Author: Chris Barton <c.barton@adelaide.edu.au>
    Date:   Thu Nov 30 10:42:23 2017 +1030
    
        It should just be false... thats dumb.
    
    commit 9a4e4d15a0596f440f54b1fbc1503a2b6964d004
    Author: Chris Barton <c.barton@adelaide.edu.au>
    Date:   Thu Nov 30 10:20:00 2017 +1030
    
        Missed out the fact that strpos returns zero, test for greater than or equal to zero.
    
    commit 9dd1d026c34f97a7ab37a5b325b9f3b998bd5bae
    Author: Chris Barton <c.barton@adelaide.edu.au>
    Date:   Mon Nov 27 16:50:39 2017 +1030
    
        Extract the switch statement into a method.
    
    commit a9ef95b71b74b9468415a6e7273c21c3bb7c0eb5
    Author: Chris Barton <c.barton@adelaide.edu.au>
    Date:   Mon Nov 27 15:33:04 2017 +1030
    
        Validate the entity form with the openshift client to verify that configruation is valid. Currently this can cause silent errors if not configured correctly.

--- web/modules/custom/shepherd/shp_orchestration/src/Form/OpenShiftConfigEntityForm.php
+++ web/modules/custom/shepherd/shp_orchestration/src/Form/OpenShiftConfigEntityForm.php
@@ -5,6 +5,8 @@ namespace Drupal\shp_orchestration\Form;
 use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use UniversityOfAdelaide\OpenShift\Client;
+use UniversityOfAdelaide\OpenShift\ClientException;
 
 /**
  * {@inheritdoc}
@@ -29,17 +31,17 @@ class OpenShiftConfigEntityForm extends EntityForm {
       '#default_value' => $entity->endpoint,
       '#required' => TRUE,
     ];
+    $form['verify_tls'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Verify TLS'),
+      '#default_value' => $entity->verify_tls,
+    ];
     $form['token'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Token'),
       '#default_value' => $entity->token,
       '#attributes' => ['autocomplete' => 'off'],
       '#required' => TRUE,
-    ];
-    $form['verify_tls'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Verify TLS'),
-      '#default_value' => $entity->verify_tls,
     ];
     $form['namespace'] = [
       '#type' => 'textfield',
@@ -68,6 +70,30 @@ class OpenShiftConfigEntityForm extends EntityForm {
   /**
    * {@inheritdoc}
    */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+    parent::validateForm($form, $form_state);
+
+    $client_response = $this->validateClientConfiguration(
+      $form['endpoint']['#value'],
+      $form['token']['#value'],
+      $form['namespace']['#value'],
+      $form['verify_tls']['#checked']
+    );
+
+    if (isset($client_response['error']) && $client_response['error']) {
+      $field_name = $this->getErrorFieldName($client_response);
+      $form_state->setErrorByName($field_name,
+        $this->t('Error: %response_code - %message', [
+          '%response_code' => $client_response['code'],
+          '%message' => !empty($client_response['message']) ? $client_response['message'] : "Can't connect, check that OpenShift is accessible.",
+        ]));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function save(array $form, FormStateInterface $form_state) {
     $entity = $this->entity;
     $status = $entity->save();
@@ -77,6 +103,76 @@ class OpenShiftConfigEntityForm extends EntityForm {
     else {
       drupal_set_message($this->t('OpenShift configuration has been saved'));
     }
+  }
+
+  /**
+   * Validates the configuration against the openshift client.
+   *
+   * @param string $endpoint
+   *   The hostname.
+   * @param string $token
+   *   A generated auth token.
+   * @param string $namespace
+   *   Namespace/project on which to operate methods on.
+   * @param bool $verify_tls
+   *   Verify tls certificates.
+   *
+   * @return array
+   *   Resource list if valid otherwise an array with error code and message.
+   */
+  protected function validateClientConfiguration($endpoint, $token, $namespace, $verify_tls) {
+    $client = new Client(
+      $endpoint,
+      $token,
+      $namespace,
+      $verify_tls
+    );
+
+    try {
+      // Make a request to the API resource list.
+      $response = $client->request('GET', '/oapi/v1/namespaces/' . $namespace);
+    }
+    catch (ClientException $e) {
+      $body = $e->getBody();
+      $response = [
+        'error' => TRUE,
+        'code' => $e->getCode(),
+        'message' => $body ? $body : $e->getMessage(),
+      ];
+    }
+
+    return $response;
+  }
+
+  /**
+   * Determines field to display the error message on based on the response.
+   *
+   * @param array $client_response
+   *   Response from the client.
+   *
+   * @return string
+   *   Field name to attach error message to.
+   */
+  protected function getErrorFieldName(array $client_response): string {
+    $field_name = '';
+    switch ($client_response['code']) {
+      // Some curl errors return a non http code - 0.
+      case FALSE:
+        // Not available at the endpoint or SSL Certificate issue.
+        $field_name = 'endpoint';
+        break;
+
+      case 401:
+        // Token is invalid.
+        $field_name = 'token';
+        break;
+
+      case 403:
+        // Cannot access namespace with token.
+        $field_name = 'namespace';
+        break;
+    }
+    return $field_name;
   }
 
 }
