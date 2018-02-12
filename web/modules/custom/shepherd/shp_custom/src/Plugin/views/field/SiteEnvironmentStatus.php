95fb9906b08cf97128667fbcb8b81206284dcdc8
Dave Mikolaj <dave@adelaide.edu.au>
2018-02-12 16:34:14 +1030

    Inject service ready for tests.

--- web/modules/custom/shepherd/shp_custom/src/Plugin/views/field/SiteEnvironmentStatus.php
+++ web/modules/custom/shepherd/shp_custom/src/Plugin/views/field/SiteEnvironmentStatus.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\shp_custom\Plugin\views\field;
 
+use Drupal\shp_orchestration\Service\Status;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 
 /**
@@ -17,7 +20,7 @@ use Drupal\views\ResultRow;
  *
  * @ViewsField("site_environment_status")
  */
-class SiteEnvironmentStatus extends FieldPluginBase {
+class SiteEnvironmentStatus extends FieldPluginBase implements ContainerFactoryPluginInterface {
 
   /**
    * {@inheritdoc}
@@ -25,13 +28,37 @@ class SiteEnvironmentStatus extends FieldPluginBase {
   public function query() {}
 
   /**
+   * Shepherd orchestration status service.
+   *
+   * @var \Drupal\shp_orchestration\Service\Status
+   *   Shepherd Orchestration status service.
+   */
+  protected $shpOrchestrationStatus;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Status $shp_orchestration_status) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->shpOrchestrationStatus = $shp_orchestration_status;
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('shp_orchestration.status')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function render(ResultRow $values) {
     // How do we get the status.
     // Use the orchestration provider to get the plugin.
-    // @todo - inject the service.
-    $environments_status = \Drupal::service('shp_orchestration.status')->get($values->_entity);
+    $environments_status = $this->shpOrchestrationStatus->get($values->_entity);
 
     // Is the environment running ?
     if ($environments_status['running']) {
