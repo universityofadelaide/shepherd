8d4cfe9cd2a688fd0757a5b523d5ebc211c97530
Dave Mikolaj <dave@adelaide.edu.au>
2018-02-13 13:42:25 +1030

    Remove extra line.

--- web/modules/custom/shepherd/shp_custom/src/Plugin/views/field/SiteEnvironmentStatus.php
+++ web/modules/custom/shepherd/shp_custom/src/Plugin/views/field/SiteEnvironmentStatus.php
@@ -8,7 +8,6 @@ use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-
 /**
  * Field handler to add the status of the environment as a whole.
  *
