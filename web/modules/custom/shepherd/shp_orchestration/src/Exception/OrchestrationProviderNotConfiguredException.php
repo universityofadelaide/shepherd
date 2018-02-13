83c72fa293822b6d2294456f6d164045dfb3fea5
Michael Priest <pingers@gmail.com>
2018-02-13 16:44:19 +1030

    Remove the orchestration provider not configured exception.

--- web/modules/custom/shepherd/shp_orchestration/src/Exception/OrchestrationProviderNotConfiguredException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\shp_orchestration\Exception;
-
-/**
- * Orchestration provider not configured exception.
- */
-class OrchestrationProviderNotConfiguredException extends \Exception {}
