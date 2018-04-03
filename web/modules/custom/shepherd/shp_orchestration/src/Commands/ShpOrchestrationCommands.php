70a99022c2686deecf758fbfdbf51b03cd1f22f0
Michael Priest <pingers@gmail.com>
2018-04-03 16:58:42 +0930

    Port Drush job queue runner command to Drush 9.

---
+++ web/modules/custom/shepherd/shp_orchestration/src/Commands/ShpOrchestrationCommands.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\shp_orchestration\Commands;
+
+use Drupal\shp_orchestration\Service\JobQueue;
+use Drush\Commands\DrushCommands;
+
+/**
+ * Shepherd Orchestration Drush commandfile.
+ */
+class ShpOrchestrationCommands extends DrushCommands {
+
+  /**
+   * The job queue.
+   *
+   * @var \Drupal\shp_orchestration\Service\JobQueue
+   */
+  protected $jobQueue;
+
+  /**
+   * ShpOrchestrationCommands constructor.
+   *
+   * @param \Drupal\shp_orchestration\Service\JobQueue $jobQueue
+   *   The job queue.
+   */
+  public function __construct(JobQueue $jobQueue) {
+    $this->jobQueue = $jobQueue;
+  }
+
+  /**
+   * Process any jobs in the job queue.
+   *
+   * @usage drush shepherd-process-job-queue
+   *   Process any jobs in the job queue
+   * @usage drush shp-p
+   *   Process any jobs in the job queue
+   *
+   * @command shepherd:process-job-queue
+   * @aliases shp-p,shepherd-process-job-queue
+   */
+  public function processJobQueue() {
+    $this->jobQueue->process();
+  }
+
+}
