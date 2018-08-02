06e3a5d0da29b08379bed3114b91d08798588eee
Michael Priest <pingers@gmail.com>
2018-08-02 10:11:36 +0930

    Feature/webops 322 demote site envs (#103)
    * Resolves site environments page dying when environment doesn't exist in OpenShift.
    
    * Restrict demotion query to only the current site.
    
    * Update OpenShift client.
    
    * Update OpenShift client again.

--- web/modules/custom/shepherd/shp_orchestration/src/Service/Environment.php
+++ web/modules/custom/shepherd/shp_orchestration/src/Service/Environment.php
@@ -306,8 +306,9 @@ class Environment extends EntityActionBase {
       ->execute();
     $promoted_term = reset(Term::loadMultiple($ids));
 
-    // Demote all current prod environments.
+    // Demote all current prod environments - for this site!
     $old_promoted = \Drupal::entityQuery('node')
+      ->condition('field_shp_site', $site->id())
       ->condition('field_shp_environment_type', $promoted_term->id())
       ->condition('nid', $environment->id(), '<>')
       ->execute();
