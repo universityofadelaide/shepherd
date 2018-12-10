d408664d48f2da1999252613c7e65428b5f2b731
Adam Bramley <adam.bramley@previousnext.com.au>
2018-12-11 09:07:12 +1100

    Remove old backup settings.

--- web/modules/custom/shepherd/shp_backup/src/Form/SettingsForm.php
+++ web/modules/custom/shepherd/shp_backup/src/Form/SettingsForm.php
@@ -70,13 +70,6 @@ class SettingsForm extends ConfigFormBase {
     ];
     $rendered_token_tree = $this->renderer->render($token_tree);
 
-    $form['root_dir'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Backup root directory'),
-      '#description' => 'Directory path to the root of the backup directory, no trailing slash required.',
-      '#default_value' => $config->get('root_dir'),
-    ];
-
     $form['backup_title'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Default backup title'),
@@ -84,32 +77,6 @@ class SettingsForm extends ConfigFormBase {
       '#default_value' => $config->get('backup_title'),
       '#element_validate' => [
         'token_element_validate',
-      ],
-    ];
-
-    $form['backup_command'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Backup command'),
-      '#description' => t('Commands to run to backup a site. Each command on a new line will be combined with && when run to stop on any error. This field supports tokens. @browse_tokens_link', ['@browse_tokens_link' => $rendered_token_tree]),
-      '#default_value' => $config->get('backup_command'),
-      '#element_validate' => [
-        'token_element_validate',
-      ],
-      '#token_types' => [
-        'shp_backup',
-      ],
-    ];
-
-    $form['restore_command'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Restore command'),
-      '#description' => t('Commands to run to restore a site. Each command on a new line will be combined with && when run to stop on any error. This field supports tokens. @browse_tokens_link', ['@browse_tokens_link' => $rendered_token_tree]),
-      '#default_value' => $config->get('restore_command'),
-      '#element_validate' => [
-        'token_element_validate',
-      ],
-      '#token_types' => [
-        'shp_backup',
       ],
     ];
 
@@ -121,11 +88,7 @@ class SettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('shp_backup.settings')
-      ->set('root_dir', '/' . trim($form_state->getValue(['root_dir']), '/'))
-      ->set('root_dir', '/' . trim($form_state->getValue(['root_dir']), '/'))
       ->set('backup_title', $form_state->getValue(['backup_title']))
-      ->set('backup_command', $form_state->getValue(['backup_command']))
-      ->set('restore_command', $form_state->getValue(['restore_command']))
       ->save();
 
     parent::submitForm($form, $form_state);
