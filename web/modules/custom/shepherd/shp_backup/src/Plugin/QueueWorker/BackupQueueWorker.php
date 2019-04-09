a3251dfbacfb905167ed281478f50b078c7a7483
Simon Lindsay <simon@iseek.biz>
2019-04-09 15:38:25 +0930

    Queue improvements (#132)
    * Queue improvements
    - If a job can't be found in openshift, assume its done and carry on.
    - Add some warnings when processing jobs to make diagnosis easier.
    * Use Interfaces, you dufus.

--- web/modules/custom/shepherd/shp_backup/src/Plugin/QueueWorker/BackupQueueWorker.php
+++ web/modules/custom/shepherd/shp_backup/src/Plugin/QueueWorker/BackupQueueWorker.php
@@ -17,13 +17,18 @@ class BackupQueueWorker extends BackupQueueWorkerBase {
    */
   public function processItem($job) {
     // Load the node for the job.
+    /** @var $backup \Drupal\node\NodeInterface */
     if ($backup = $this->nodeStorage->load($job->backupId)) {
       // Perform the job.
       if ($responseBody = $this->backup->create($backup)) {
         // Update the job name for isComplete() check.
         $this->setJobName($job, $responseBody);
+        return;
       }
     }
+
+    // If we get to here, something is going wrong, alert about the backup that is an issue.
+    \Drupal::logger('shp_backup')->error('An error occurred processing: %backup', ['%backup' => $job->backupId]);
   }
 
 }
