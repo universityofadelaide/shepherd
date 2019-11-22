3ea5e32a496b077ac7e233aa23a6861e6448ec73
Michael Priest <pingers@gmail.com>
2019-11-22 15:38:45 +1030

    Start tests for Provisioner.

---
+++ web/modules/custom/shepherd/shp_database_provisioner/tests/src/Unit/ProvisionerTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Drupal\Tests\shp_database_provisioner\Unit;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\shp_custom\Service\Environment;
+use Drupal\shp_custom\Service\StringGenerator;
+use Drupal\shp_database_provisioner\Service\Provisioner;
+use Drupal\shp_orchestration\OrchestrationProviderInterface;
+use Drupal\shp_orchestration\OrchestrationProviderPluginManagerInterface;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Tests the OrchestrationEvents class constants.
+ *
+ * @group shepherd
+ * @group shp_database_provisioner
+ *
+ * @coversDefaultClass \Drupal\shp_database_provisioner\Service\Provisioner
+ */
+class ProvisionerTest extends UnitTestCase {
+
+  /**
+   * @var \Drupal\shp_database_provisioner\Service\Provisioner
+   */
+  protected $provisioner;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $config_factory = $this->prophesize(ConfigFactoryInterface::class);
+    $orchestration_provider_manager = $this->prophesize(OrchestrationProviderPluginManagerInterface::class);
+    $environment = $this->prophesize(Environment::class);
+    $string_generator = $this->prophesize(StringGenerator::class);
+
+    $orchestration_provider = $this->prophesize(OrchestrationProviderInterface::class);
+    $orchestration_provider_manager->getProviderInstance()
+      ->willReturn($orchestration_provider->reveal());
+
+    $this->provisioner = new Provisioner(
+      $config_factory->reveal(),
+      $orchestration_provider_manager->reveal(),
+      $environment->reveal(),
+      $string_generator->reveal()
+    );
+  }
+
+  /**
+   * Test the constants.
+   *
+   * @covers ::createDatabase
+   *
+   * @dataProvider createDatabaseInputData
+   */
+  public function testCreateDatabase($input, $expected_output) {
+    $dbMock = $this->getMockBuilder('mysqli')
+      ->getMock();
+    $dbMock->expects($this->once())
+      ->method('prepare')
+      ->with($expected_output);
+    $this->provisioner->createDatabase($input, $dbMock);
+
+  }
+
+  /**
+   * Test input data for sanitise test.
+   */
+  public function createDatabaseInputData() {
+    return [
+      ['abc123', 'CREATE DATABASE `abc123`'],
+    ];
+  }
+
+  /**
+   * Test the constants.
+   *
+   * @covers ::createUser
+   *
+   * @dataProvider createUserInputData
+   */
+  public function testCreateUser($database, $username, $password, $expected_output) {
+    $dbMock = $this->getMockBuilder('mysqli')
+      ->getMock();
+    $dbMock->expects($this->once())
+      ->method('prepare')
+      ->with($expected_output);
+    $this->provisioner->createUser($database, $username, $password, $dbMock);
+
+  }
+
+  /**
+   * Test input data for sanitise test.
+   */
+  public function createUserInputData() {
+    return [
+      [
+        'foo',
+        'bar',
+        'meh',
+        'GRANT ALL PRIVILEGES ON `foo`.* TO `bar`@`%` IDENTIFIED BY \'meh\'',
+      ]
+    ];
+  }
+
+}
