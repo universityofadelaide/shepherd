644f371b1247f732835e8982618ce7abeb5ded68
Adam Bramley <adam.bramley@previousnext.com.au>
2020-04-29 13:57:38 +1000

    Only reindex when theres ports

--- web/modules/custom/shepherd/shp_cache_backend/src/Plugin/CacheBackend/MemcachedDatagrid.php
+++ web/modules/custom/shepherd/shp_cache_backend/src/Plugin/CacheBackend/MemcachedDatagrid.php
@@ -478,8 +478,10 @@ class MemcachedDatagrid extends CacheBackendBase {
       if (empty($container['ports'])) {
         unset($container['ports']);
       }
-      // Reindex the array.
-      $container['ports'] = array_values($container['ports']);
+      else {
+        // Reindex the array.
+        $container['ports'] = array_values($container['ports']);
+      }
     }
 
     $statefulSet->setSpec($spec);
