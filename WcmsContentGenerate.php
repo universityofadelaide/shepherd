4bedb62b0ac6f682ae3b6fea2832122f9ee43709
Simon Lindsay <simon@iseek.biz>
2022-09-01 15:55:15 +0930

    Separators handled automatically now.

--- WcmsContentGenerate.php
+++ WcmsContentGenerate.php
@@ -43,7 +43,7 @@ $openshift_config = [
   'endpoint'   => $openshift_url,
   'token'      => $token,
   'namespace'          => 'shepherd-dev',
-  'site_deploy_prefix' => 'shepherd-dev-',
+  'site_deploy_prefix' => 'shepherd-dev',
   'verify_tls' => FALSE,
 ];
 $orchestration_config = $config->getEditable('shp_orchestration.settings');
