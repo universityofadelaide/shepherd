682a303fd925d8764957be72139a6c2fc9d9bc8f
Simon Lindsay <simon@iseek.biz>
2022-11-04 17:40:06 +1030

    Switch from random to least used service account selection. (#207)
    * Switch from random to least used service account selection.
    
    * Thanks again linty mc linter.
    
    * Use the generic getServiceAccount method.
    
    * Remove not so public methods from the interface.
    
    * Yep, we need a site id for getting a service account.
    
    * Fix return value type.
    
    * Set service account by default for testing.
    
    * Pass through the right service account for the site.
    
    * Fixes missing label.
    
    * Simplify logic.
    
    Co-authored-by: Michael Priest <pingers@gmail.com>

--- web/modules/custom/shepherd/shp_service_accounts/src/Service/ServiceAccounts.php
+++ web/modules/custom/shepherd/shp_service_accounts/src/Service/ServiceAccounts.php
@@ -34,37 +34,80 @@ class ServiceAccounts implements ServiceAccountsInterface {
   public function getServiceAccount($siteId): ?ServiceAccount {
     /** @var \Drupal\node\Entity\Node $site */
     $site = $this->entityTypeManager->getStorage('node')->load($siteId);
-
     if (!$site) {
       throw new SiteNotFoundException("Site not found.");
     }
 
-    if (!isset($site->field_shp_service_account->value)) {
-      $serviceAccount = $this->getRandomServiceAccount();
+    if (isset($site->field_shp_service_account->value)) {
+      return $this->getServiceAccountByName($site->field_shp_service_account->value);
     }
-    else {
-      $serviceAccount = $this->getServiceAccountByName($site->field_shp_service_account->value);
-    }
-
-    return $serviceAccount;
+    return $this->getLowestServiceAccount();
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getRandomServiceAccount(): ?ServiceAccount {
+  public function getServiceAccounts(): ?array {
     $serviceAccountList = $this->entityTypeManager->getStorage('service_account')->loadByProperties([
       'status' => TRUE,
     ]);
 
     $entries = count($serviceAccountList);
-    if ($entries == 0) {
+    if ($entries === 0) {
       throw new NoServiceAccountException("No service accounts defined.");
     }
 
+    return $serviceAccountList;
+  }
+
+  /**
+   * Retrieve the list of service accounts and return a random one.
+   *
+   * Random as otherwise we need to keep track of the most recently
+   * used one, wrap around, etc.
+   *
+   * @throws \Exception
+   *   If there are no service accounts defined.
+   *
+   * @return \Drupal\shp_service_accounts\Entity\ServiceAccount|null
+   *   The ServiceAccount or NULL
+   */
+  public function getRandomServiceAccount(): ?ServiceAccount {
+    $serviceAccountList = $this->getServiceAccounts();
+
     /** @var \Drupal\shp_service_accounts\Entity\ServiceAccount */
-    $serviceAccount = array_values($serviceAccountList)[random_int(0, $entries - 1)];
-    return $serviceAccount;
+    return array_values($serviceAccountList)[random_int(0, count($serviceAccountList) - 1)];
+  }
+
+  /**
+   * Retrieve the list of service accounts and return the least used one.
+   *
+   * @throws \Exception
+   *   If there are no service accounts defined.
+   *
+   * @return \Drupal\shp_service_accounts\Entity\ServiceAccount|null
+   *   The ServiceAccount or NULL
+   */
+  public function getLowestServiceAccount(): ?ServiceAccount {
+    $serviceAccountDist = [];
+    $serviceAccountList = $this->getServiceAccounts();
+
+    foreach ($serviceAccountList as $serviceAccount) {
+      $query = $this->entityTypeManager->getStorage('node')
+        ->getQuery()
+        ->accessCheck(FALSE);
+
+      $query->condition('type', 'shp_site');
+      $query->condition('field_shp_service_account', $serviceAccount->label());
+      $serviceAccountDist[$serviceAccount->label()] = count($query->execute());
+    }
+
+    asort($serviceAccountDist);
+
+    $array = array_keys($serviceAccountDist);
+    $lowestAccount = reset($array);
+
+    return $this->getServiceAccountByName($lowestAccount);
   }
 
   /**
@@ -81,8 +124,7 @@ class ServiceAccounts implements ServiceAccountsInterface {
     }
 
     /** @var \Drupal\shp_service_accounts\Entity\ServiceAccount */
-    $serviceAccount = reset($serviceAccountList);
-    return $serviceAccount;
+    return reset($serviceAccountList) ?: NULL;
   }
 
 }
